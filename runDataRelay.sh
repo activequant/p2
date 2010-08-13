49d8eb50958c58f034aab0ccd07f2db5724b12cc
ustaudinger <ustaudinger@67d100d7-d997-4361-b953-07cc37fa8f36>
2010-08-13 15:33:01 +0000

    Fixing startup script
    git-svn-id: svn://activequant.com/svn/repositories/hidden/trunk/p2@258 67d100d7-d997-4361-b953-07cc37fa8f36

--- runDataRelay.sh
+++ runDataRelay.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Runs the data relay  
-cd /home/ustaudinger/p2-work/activequant-p2
+cd /home/ustaudinger/p2-work/p2-trunk/p2
 java -classpath target/activequant-p2-1.3-SNAPSHOT-jar-with-dependencies.jar:target/ -DJMS_HOST=localhost -DJMS_PORT=7676 -DARCHIVE_BASE_FOLDER=/var/www/archive2 org.activequant.util.DataRelay
